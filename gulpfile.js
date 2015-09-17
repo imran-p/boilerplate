ce4878b725c4f16c7a6f2cc46b381b32f946bc07
Imran Parkar <imranparkar@outlook.com>
2015-09-18 00:11:56 +0100

    First commit: added files & folders

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// Dependencies
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync');
+
+// Tasks
+gulp.task('styles', function() {
+  return gulp.src('scss/global.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+  	.pipe(sourcemaps.write())
+  	.pipe(gulp.dest('css/'))
+    .pipe(concat('global.css'))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('css/'))
+});
+
+
+gulp.task('jshint', function() {
+    return gulp.src('js/main/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['js/libs/*.js', 'js/main/*.js'])
+    .pipe(concat('global.js'))
+    .pipe(gulp.dest('js/dist/'))
+    .pipe(rename('global.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/dist/'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/global.css','js/dist/global.js'], {
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['styles']);
+    gulp.watch('js/main/*.js', ['jshint', 'scripts']);
+    gulp.watch(['*.html'], ['bs-reload']);
+});
+
+// Default task
+gulp.task('default', ['styles', 'jshint', 'scripts', 'browser-sync', 'watch']);
